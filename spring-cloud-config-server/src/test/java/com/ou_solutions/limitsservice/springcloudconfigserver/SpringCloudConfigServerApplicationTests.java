71a3a7fe3cf05e27919a977f908ee2bc8cb5762d
omkarupa <omkarmh009@gmail.com>
2023-06-25 17:32:48 +0800

    First commit of Spring Cloud Server

---
+++ spring-cloud-config-server/src/test/java/com/ou_solutions/limitsservice/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ou_solutions.limitsservice.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
